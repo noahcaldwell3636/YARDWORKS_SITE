6a8e93fe144ab66074ff74e17f74e1f5fc936e04
Noah Caldwell <ncaldwell@YardworksLLC.local>
2020-07-13 16:31:46 -0400

    Form actually works now, didn't work as of last commit (I didn't check)

--- YW_site/static/js/SheetSubmissionFunctions.js
+++ YW_site/static/js/SheetSubmissionFunctions.js
@@ -1,33 +1,3 @@
-$.ajaxSetup({ 
-    beforeSend: function(xhr, settings) {
-        function getCookie(name) {
-            var cookieValue = null;
-            if (document.cookie && document.cookie != '') {
-                var cookies = document.cookie.split(';');
-                for (var i = 0; i < cookies.length; i++) {
-                    var cookie = jQuery.trim(cookies[i]);
-                    // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                        alert('good!')
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
-                    } else {
-                        alert("Bad!")
-                    }
-                }
-            }
-            return cookieValue;
-        }
-        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-        // Only send the token to relative URLs i.e. locally.
-        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
-    } 
-});
-
-
-
-
 
 function addSection() {
     // const csrftoken = Cookies.get('csrftoken');
@@ -59,6 +29,7 @@ function disableButton() {
 
 function submitForms() {
     var xhttp = new XMLHttpRequest();
+    var form_data = new FormData();
     for(var i=0, n=document.forms.length; i<n; i++){
         form = document.forms[i];
         form_els = document.forms[i].elements;
@@ -69,33 +40,15 @@ function submitForms() {
         var image = form_els[4];
         var url = window.location.href;
         
-
         xhttp.open("POST", url, true);
         xhttp.setRequestHeader('X-CSRFToken', csrf.value); 
-        var form_data = new FormData();
         form_data.append("submitter", submitter.value);
         form_data.append("date", date.value);
         form_data.append("equipment", equipment_id.value);
         form_data.append("image", image.value);
         form_data.append("sub", submitter.value);
-        xhttp.send(form_data);
     }
-}
-
-function unhideSheetSubmission() {
-    hidden_elements = document.getElementsByClassName("sheet-form")
-    for (var i = 0; i < hidden_elements.length; i++) {
-        hidden_elements[i].style.visibility = "visible";
-    }
-}
-
-function hideSheetSubmission() {
-    alert('Yo!')
-    hidden_elements = document.getElementsByClassName("sheet-form")
-    for (var i = 0; i < hidden_elements.length; i++) {
-        alert("hit loop!")
-        hidden_elements[i].style.visibility = "hidden";
-    }
+    xhttp.send(form_data);
 }
 
 
